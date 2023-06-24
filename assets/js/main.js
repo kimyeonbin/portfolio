d47df9400eedca393e6109761f481d4d0291f6ee
357642 <dusqls76@naver,com>
2023-06-24 15:24:35 +0900

    js 정리

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,42 @@
 $(function(){
-    
     gsap.registerPlugin(ScrollTrigger);
-    
     ScrollTrigger.saveStyles(".mobile, .desktop");
-    
     ScrollTrigger.matchMedia({
-        
         "all": function() {
-            /**
-             * @커서
-             */
+            // 커서
             $(window).mousemove(function(e){
                 e.preventDefault();
                 gsap.to('.cursor',{
-                    x:e.clientX,
-                    y:e.clientY,
-                    duration:0,
+                    x: e.clientX,
+                    y: e.clientY,
+                    duration: 0,
                 });
             });
             $('.gnb button').mouseover(function(){
-                gsap.to('.cursor',{scale:3});
+                gsap.to('.cursor',{scale: 3});
             });
             $('.gnb button').mouseleave(function(){
-                gsap.to('.cursor',{scale:1});
+                gsap.to('.cursor',{scale: 1});
             });
             $('.footer .contact-area a').mouseover(function(){
-                gsap.to('.cursor', {mixBlendMode:"normal", scale:0.4});
+                gsap.to('.cursor', {mixBlendMode: "normal", scale: 0.4});
             });
             $('.footer .contact-area a').mouseleave(function(){
-                gsap.to('.cursor', {mixBlendMode:"difference", scale:1});
+                gsap.to('.cursor', {mixBlendMode: "difference", scale: 1});
             });
             $('.sc-work .thumb-area').mouseover(function(){
-                gsap.to('.cursor', {mixBlendMode:"normal"});
+                gsap.to('.cursor', {mixBlendMode: "normal"});
             });
             $('.sc-work .thumb-area').mouseleave(function(){
-                gsap.to('.cursor', {mixBlendMode:"difference"});
+                gsap.to('.cursor', {mixBlendMode: "difference"});
             });
             $('.sc-work .more-box a').mouseover(function(){
-                gsap.to('.cursor', {scale:0.4});
+                gsap.to('.cursor', {scale: 0.4});
             });
             $('.sc-work .more-box a').mouseleave(function(){
-                gsap.to('.cursor', {scale:1});
+                gsap.to('.cursor', {scale: 1});
             });
-            /**
-             * @비주얼텍스트_나타남
-             */
+            // 비주얼텍스트_나타남
             gsap.timeline({
                 paused: true,
             })
@@ -60,29 +52,25 @@ $(function(){
                 rotateY: 0,
                 opacity: 1,
                 stagger: 0.5,
-                delay:1.3,
+                delay: 1.3,
             })
             .play();
-            /**
-             * @어바웃텍스트_나타남
-             */
+            // 어바웃텍스트_나타남
             gsap.timeline({
                 paused: true,
             })
             .fromTo('.sc-about .desc',{
                 opacity: 0,
-                y:30,
+                y: 30,
                 delay: 4.5,
             },{
+                duration: 1,
                 opacity: 1,
                 delay: 4.5,
-                duration: 1,
-                y:0,
+                y: 0,
             })
             .play();
-            /**
-             * @상단이미지_움직임
-             */
+            // 상단이미지_움직임
             gsap.from('.container .illu.flo1', {
                 duration: 1,
                 scale: (1.3),
@@ -90,12 +78,12 @@ $(function(){
             });
             gsap.to('.container .illu.flo1', {
                 duration: 1,
-                right:-30,
+                right: -30,
                 delay: 3,
             });
             gsap.from('.container .illu.flo2', {
-                rotate: 9,
                 duration: 1.5,
+                rotate: 9,
                 scale: (1.7),
             });
             gsap.from('.container .illu.flo3', {
@@ -108,32 +96,28 @@ $(function(){
                 repeat: -1,
                 yoyo: true,
                 ease: "linear",
-                delay:1,
+                delay: 1,
             });
             gsap.from('.container .illu.flo4', {
-                rotate: -10,
                 duration: 1.5,
+                rotate: -10,
                 scale: (2),
             });
             gsap.to('.container .illu.flo4', {
-                rotate: 30,
                 duration: 1.5,
+                rotate: 30,
             }); 
-            /**
-             * @gnb_버튼
-             */
+            // gnb_버튼
             $('.gnb button').click(function(){
     
-                target=$(this).data('target');
-                targetPos=$(target).offset().top;
+                target = $(this).data('target');
+                targetPos = $(target).offset().top;
 
-                window.scrollTo({top:targetPos,behavior:"smooth"});
+                window.scrollTo({top: targetPos, behavior: "smooth"});
             });
-            /**
-             * @상단으로올라가는_버튼
-             */
+            // 상단으로올라가는_버튼
             $(window).scroll(function(){
-                curr=$(this).scrollTop();
+                curr = $(this).scrollTop();
                 if(curr >= 20){
                     $('.fix-btn .btn-top').addClass('show');
                 }else{
@@ -141,17 +125,14 @@ $(function(){
                 };
             });
             $('.fix-btn .btn-top').click(function(){
-                window.scrollTo({top:0,behavior:"smooth"});
+                window.scrollTo({top: 0, behavior: "smooth"});
             });
-            /**
-             * @진행률표시바_인디케이터
-             */
+            // 진행률표시바_인디케이터
             window.onscroll = function(){myFunction()};
-
             function myFunction(){
                 var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
                 var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-                var scrolled = (winScroll/height)*100;
+                var scrolled = (winScroll / height) * 100;
                 document.getElementById("indicator").style.width = scrolled + "%";
             };           
         }
